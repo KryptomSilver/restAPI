33086b27479275d37469f4cac47410bda01579a1
KryptomSilver <abelromeror763@gmail.com>
2024-06-19 14:26:56 -0600

    feat: added database config

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config({ path: '.env' })
+const { checkDB } = require("./src/database.config")
 
+checkDB()
+
+const PORT = process.env.APP_PORT | 3000
 const app = express()
 
 app.get("/", (req, res) => {
   res.send("hola")
 })
 
-app.listen(5000, () => {
-  console.log("server on port 6000")
+app.listen(PORT, () => {
+  console.log(`server on port ${PORT}`)
 })
---
+++ src/database.config.js
@@ -0,0 +1,22 @@
+const { createPool } = require("mysql2/promise");
+
+const pool = createPool({
+  host: `${process.env.DB_HOST}`,
+  user: `${process.env.DB_USER}`,
+  password: `${process.env.DB_PASSWORD}`,
+  database: `${process.env.DB_NAME}`,
+})
+
+const checkDB = async () => {
+  try {
+    await pool.query("SELECT 1")
+    console.log("DB is connected")
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = {
+  checkDB,
+  pool
+}
